a446aa6a6f70c1c9d09bbfe7b4fa015116e4a2d4
harit <harit.himanshu@yahoo.in>
2015-12-01 15:17:53 -0800

    time server completed. test to follow

--- build.sbt
+++ build.sbt
@@ -10,5 +10,6 @@ import sbt._
 lazy val root = (project in file("."))
   .settings(libraryDependencies ++= Seq(
     "com.typesafe.akka" % "akka-actor_2.11" % "2.4-SNAPSHOT",
-    "com.typesafe.akka" % "akka-http-experimental_2.11" % "2.0-M2"
+    "com.typesafe.akka" % "akka-http-experimental_2.11" % "2.0-M2",
+    "com.typesafe.akka" %% "akka-http-spray-json-experimental" % "2.0-M2"
   ))
--- src/main/scala/com/akka/http/time/Server.scala
+++ src/main/scala/com/akka/http/time/Server.scala
@@ -1,5 +1,43 @@
 package com.akka.http.time
 
-class Server {
+import java.time.{LocalDateTime, LocalTime}
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import spray.json._
+import scala.io.StdIn.readLine
+
+final case class Time(now: String = LocalDateTime.now().toString)
+
+trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val timeFormat = jsonFormat1(Time)
 }
+
+object Server extends App with JsonSupport {
+  implicit val system = ActorSystem("timeServer")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val ec = system.dispatcher
+
+
+  val route =
+    pathSingleSlash {
+      get {
+        complete {
+          Time()
+        }
+      }
+    }
+
+  val serverFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+  println(s"TimeServer is running at http://localhost:8080\nPress RETURN to stop ...")
+  readLine()
+
+  serverFuture
+    .flatMap(_.unbind())
+    .onComplete(_ => system.terminate())
+}
+
